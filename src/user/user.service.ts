23e4cb4e3d7f0e5d72583201fe43e6bdaaceefbc
Gustavo Freitas <gustavoalgfreitas@hotmail.com>
2026-04-10 18:14:43 -0300

    Inclusão de função GET

---
+++ src/user/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find({
+      select: ['id', 'name', 'email', 'phoneNumber', 'createdAt', 'updatedAt'],
+    });
+  }
+}
